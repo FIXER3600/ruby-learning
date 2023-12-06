187a00c9f1fe458968530d2385a3d77b56edd5f9
FIXER3600 <guilhermecomercial51@gmail.com>
2023-12-05 21:51:44 -0300

    test:pangram

--- solv_problems/find_three.rb
+++ solv_problems/find_three.rb
@@ -1,8 +1,10 @@
 arr= [10, 4, 3, 50, 230, 90] 
+class Finder
 def find_three(array)
 	array.sort { |a, b| b <=> a }
 	array.reverse!
 	return "Os 3 maiores valores são #{array[0]}, #{array[1]} e #{array[2]}"
 end
-
-puts find_three(arr)
+end
+finder=Finder.new
+puts finder.find_three(arr)
